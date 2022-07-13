0caaa4b446a71666ae5dbd9181c0e3662600aeb3
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2022-07-13 18:56:48 -0300

    Tudo funcionando até aqui
    Observar a alteração do skd para minSdkVersion 23 e do settings.gradle

--- lib/tabs/home_tabs.dart
+++ lib/tabs/home_tabs.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeTab extends StatelessWidget {
   const HomeTab({Key? key}) : super(key: key);
@@ -31,6 +34,43 @@ class HomeTab extends StatelessWidget {
                 title: Text('Novidades'),
                 centerTitle: true,
               ),
+            ),
+            FutureBuilder<QuerySnapshot>(
+              future: Firestore.instance
+                  .collection('produtos')
+                  .orderBy('pos')
+                  .getDocuments(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  );
+                } else {
+                  return SliverStaggeredGrid.count(
+                    crossAxisCount: 4,
+                    mainAxisSpacing: 0,
+                    crossAxisSpacing: 0,
+                    staggeredTiles: snapshot.data!.documents.map((doc) {
+                      return StaggeredTile.count(doc.data['x'], doc.data['y']);
+                    }).toList(),
+                    children: snapshot.data!.documents.map(
+                      (doc) {
+                        return FadeInImage.memoryNetwork(
+                          placeholder: kTransparentImage,
+                          image: doc.data['image'],
+                          fit: BoxFit.cover,
+                        );
+                      },
+                    ).toList(),
+                  );
+                }
+              },
             )
           ],
         )

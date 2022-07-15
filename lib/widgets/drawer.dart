3df7a2fc90ba347eda1ba3bb0d3c1ac6bd79bbbd
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2022-07-15 15:31:05 -0300

    Design do Drawer finalizado
    Design ok
    Falta a parte funcional para clicar e mudar de tela

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -27,7 +27,7 @@ class CustomDrawer extends StatelessWidget {
               Container(
                 margin: EdgeInsets.only(bottom: 8),
                 padding: EdgeInsets.fromLTRB(0, 16, 16, 8),
-                height: 170,
+                height: MediaQuery.of(context).size.height,
                 child: Stack(
                   children: [
                     Column(
@@ -48,11 +48,90 @@ class CustomDrawer extends StatelessWidget {
                           child: Text(
                             'Store',
                             style: TextStyle(
-                              fontSize: 20,
+                              fontSize: 14,
                               fontWeight: FontWeight.w400,
                             ),
                           ),
                         ),
+                        SizedBox(height: 20),
+                        Container(
+                          alignment: Alignment.topLeft,
+                          margin: EdgeInsets.only(top: 20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Olá,',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              GestureDetector(
+                                child: Text(
+                                  'Entre ou se cadastre',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Theme.of(context).primaryColor),
+                                ),
+                                onTap: () {},
+                              )
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          child: ListView(
+                            children: <Widget>[
+                              ListTile(
+                                contentPadding:
+                                    EdgeInsets.zero, //tira o espaço padrão
+                                title: Row(
+                                  children: <Widget>[
+                                    Icon(Icons.home),
+                                    SizedBox(width: 20),
+                                    Text("Início")
+                                  ],
+                                ),
+                                onTap: () {},
+                              ),
+                              ListTile(
+                                contentPadding:
+                                    EdgeInsets.zero, //tira o espaço padrão
+                                title: Row(
+                                  children: <Widget>[
+                                    Icon(Icons.list),
+                                    SizedBox(width: 20),
+                                    Text("Produtos")
+                                  ],
+                                ),
+                                onTap: () {},
+                              ),
+                              ListTile(
+                                contentPadding:
+                                    EdgeInsets.zero, //tira o espaço padrão
+                                title: Row(
+                                  children: <Widget>[
+                                    Icon(Icons.location_pin),
+                                    SizedBox(width: 20),
+                                    Text("Lojas")
+                                  ],
+                                ),
+                                onTap: () {},
+                              ),
+                              ListTile(
+                                contentPadding:
+                                    EdgeInsets.zero, //tira o espaço padrão
+                                title: Row(
+                                  children: <Widget>[
+                                    Icon(Icons.playlist_add_check),
+                                    SizedBox(width: 20),
+                                    Text("Meus pedidos")
+                                  ],
+                                ),
+                                onTap: () {},
+                              ),
+                            ],
+                          ),
+                        )
                       ],
                     ),
                   ],

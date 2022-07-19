98bc5a4811ef3f269737f8b0c755c047730071e8
albertobezerra <albertofbezerra@gmail.com>
2022-07-19 12:19:51 -0300

    Preparação para receber os tipo de produtos do Firebase

--- lib/screens/produtos_screen.dart
+++ lib/screens/produtos_screen.dart
@@ -1,5 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:storebypaulasaraiva/data/produtos_data.dart';
+
+import '../tiles/produtos_tile.dart';
 
 class ProdutosScreen extends StatelessWidget {
   final DocumentSnapshot snapshot;
@@ -9,35 +12,72 @@ class ProdutosScreen extends StatelessWidget {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        appBar: AppBar(
-          elevation: 4,
-          backgroundColor: Color.fromARGB(255, 211, 118, 130),
-          title: Text(snapshot.data['title']),
-          centerTitle: true,
-          bottom: TabBar(
-            indicatorColor: Colors.white,
-            tabs: [
-              Tab(
-                icon: Icon(Icons.grid_on),
-              ),
-              Tab(
-                icon: Icon(Icons.list),
-              )
-            ],
-          ),
-        ),
-        body: TabBarView(
-          physics: NeverScrollableScrollPhysics(),
-          children: [
-            Container(
-              color: Colors.pink,
+          appBar: AppBar(
+            elevation: 4,
+            backgroundColor: Color.fromARGB(255, 211, 118, 130),
+            title: Text(snapshot.data['title']),
+            centerTitle: true,
+            bottom: TabBar(
+              indicatorColor: Colors.white,
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.grid_on),
+                ),
+                Tab(
+                  icon: Icon(Icons.list),
+                )
+              ],
             ),
-            Container(
-              color: Colors.pinkAccent,
-            )
-          ],
-        ),
-      ),
+          ),
+          body: FutureBuilder<QuerySnapshot>(
+            future: Firestore.instance
+                .collection('produtos')
+                .document(snapshot.documentID)
+                .collection('Tipos')
+                .getDocuments(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                      Color.fromARGB(255, 211, 118, 130),
+                    ),
+                  ),
+                );
+              } else {
+                return TabBarView(
+                  physics: NeverScrollableScrollPhysics(),
+                  children: [
+                    GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisExtent: 4,
+                        crossAxisSpacing: 4,
+                        childAspectRatio: 0.65,
+                      ),
+                      itemCount: snapshot.data!.documents.length,
+                      itemBuilder: (context, index) {
+                        return ProdutosTile(
+                            'grid',
+                            ProdutosData.fromDocument(
+                                snapshot.data!.documents[index]));
+                      },
+                    ),
+                    ListView.builder(
+                      padding: EdgeInsets.all(4),
+                      itemCount: snapshot.data!.documents.length,
+                      itemBuilder: (context, index) {
+                        return ProdutosTile(
+                            'lista',
+                            ProdutosData.fromDocument(
+                                snapshot.data!.documents[index]));
+                      },
+                    )
+                  ],
+                );
+              }
+            },
+          )),
     );
   }
 }
---
+++ lib/tiles/produtos_tile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:storebypaulasaraiva/data/produtos_data.dart';
+
+class ProdutosTile extends StatelessWidget {
+  final String type;
+  final ProdutosData data;
+
+  ProdutosTile(this.type, this.data);
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

9730c485f88e507eeb0364e154e391e2f0ee7414
albertobezerra <albertofbezerra@gmail.com>
2022-07-20 09:09:48 -0300

    Finalização da tela individual de cada produto

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -61,6 +61,15 @@ class _ProdutcScreenState extends State<ProdutcScreen> {
                 ),
                 SizedBox(height: 16),
                 Text(
+                  'Descrição do Produto',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  product.description,
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.w300),
+                ),
+                SizedBox(height: 16),
+                Text(
                   'Tamanho',
                   style: TextStyle(
                     fontSize: 16,
@@ -99,7 +108,22 @@ class _ProdutcScreenState extends State<ProdutcScreen> {
                       );
                     }).toList(),
                   ),
-                )
+                ),
+                SizedBox(height: 16),
+                SizedBox(
+                  height: 44,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      primary: Colors.white,
+                      backgroundColor: Color.fromARGB(255, 211, 118, 130),
+                      onSurface: Colors.grey,
+                    ),
+                    onPressed: size != null ? () {} : null,
+                    child: Text(
+                      'Adicionar ao carinho',
+                    ),
+                  ),
+                ),
               ],
             ),
           )

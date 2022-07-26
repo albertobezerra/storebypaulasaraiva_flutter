e08123a66e60c7b2bd421bad8407a36e93e625bd
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2022-07-25 23:41:36 -0300

    Update user_model.dart

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class UserModel extends Model {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  late FirebaseUser firebaseUser;
+
+  Map<String, dynamic> userData = Map();
+
   bool isLoading = false;
 
   void signUp() {}
